5b625f0bc265f55984eba3ec18fc338d4fe08180
Fucking Sasha <90604394+SbeNaga@users.noreply.github.com>
2022-12-27 14:32:58 +0300

    Add files via upload

---
+++ Js-start/script.js
@@ -0,0 +1,21 @@
+let arr = ['yes',2,3];
+let b = 2;
+let area = document.querySelector("return ds")
+let a = function()
+{
+    let res = 0;
+    res = prompt("Введите b дискреминант") ** 2;
+    return res
+    
+}
+
+function sep()
+{
+    let dis = 0;
+    dis =  a() - 4 * prompt("Введите a дискреминант") * prompt("Введите c дискреминант");
+    return area.addEventListener("mouseover")(dis)
+    if(dis == Nan) 
+    {
+       return alert("вы ввели не возможное значение a или b")
+    }
+}
